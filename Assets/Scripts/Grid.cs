ecd954c4f7c715b47aa5e0ddfa794a4bbb0d6ee3
kweeks <kweeksdev@gmail.com>
2017-10-23 16:12:06 -0400

    Skybox and new folders

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+* Draws the grid the user's will see
+*/
+public class Grid : MonoBehaviour
+{
+    public int gridSize = 3; // refers to the length and width of a grid ie NxN
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+* Base Ship Class that defines the ships movement, attack, and abilities
+*/ 
+public class Ship : MonoBehaviour
+{
+    public int health = 3;
+    public int shield = 3;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
