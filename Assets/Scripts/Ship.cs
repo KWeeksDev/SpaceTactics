ea6692c44f15516955b85cb24b70b3273b7f5971
Kyle Weeks <kweeksdev@gmail.com>
2017-11-10 15:01:51 -0500

    Added Game Manager and first pass ship selection

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -8,16 +8,53 @@ public class Ship : MonoBehaviour
 {
     public int health = 3;
     public int shield = 3;
+	public bool isSelected = false; // In future we'll have different behaviour depending which player is selecting the ship
+	private Renderer rend;
+	private Shader shaderStandard;
+	private Shader shaderOutline; 
 
 	// Use this for initialization
 	void Start ()
     {
-		
+		shaderStandard  = Shader.Find("Standard");
+		shaderOutline = Shader.Find("Custom/Silhouette-Outline");
+		rend = GetComponent<Renderer>();
+		rend.material.shader = shaderStandard;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
 		
+	}
+
+	// Modifies this object when selected by a player
+	public void Select()
+	{
+		if (isSelected == true)
+		{
+			Debug.Log("Ship is already selected");
+			return;
+		}
+
+		isSelected = true;
+
+		rend.material.shader = shaderOutline;
+
+
+	}
+
+	public void Deselect()
+	{
+		if(isSelected == false)
+		{
+			Debug.Log("Ship is already unselected");
+			return;
+		}
+
+		isSelected = false;
+
+		rend.material.shader = shaderStandard;
+
 	}
 }
